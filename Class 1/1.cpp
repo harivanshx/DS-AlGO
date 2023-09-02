e5b103bf1a20a6573276f80369aad738a067b7a4
Harivansh Bhardwaj <harivanshbhardwaj57@gmail.com>
2023-09-02 06:55:58 +0530

    Initial commit

---
+++ Class 1/1.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+int main(){
+    std::cout<<"Harivansh Bhardwaj";
+
+
+    return 0;
+    }
---
+++ Class 1/2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    cout<<"Harivansh Bhardwaj"<<endl;
+
+    return 0;
+
+}
